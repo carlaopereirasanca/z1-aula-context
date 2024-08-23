62626f357fa3e6b8981a3ccff5da0433af2c4892
Carlos Jose Pereira <carlao2005@gmail.com>
2024-08-23 15:50:55 -0300

    completo sem context

--- src/components/CartPage.js
+++ src/components/CartPage.js
@@ -1,16 +1,38 @@
+import SingleProduct from "./SingleProduct";
+import './styles.css';
 
-const CartPage = () => {
+const CartPage = ( {carrinho, setCarrinho} ) => {
+
+    // Calcula a quantidade e o valor total dos produtos
+    // que estão no carrinho.
+
+    var valorTotal = 0;
+    carrinho.map( (prod) => {
+        valorTotal = valorTotal + Number(prod.price);
+    } )
+    var qtdProdutos = carrinho.length;
 
     return (
 
         <div>
             <h2>Carrinho de Compras</h2>
-            <h3>Total: R$ 230,00</h3>
+            <h3>Produtos: {qtdProdutos} Total: R$ {valorTotal}</h3>
             <div className='productContainer'>
 
+                { carrinho.map( (prod) => {
+                    return (
+                        <SingleProduct
+                            key={prod.id} 
+                            prod={prod} 
+                            carrinho={carrinho}
+                            setCarrinho={setCarrinho}
+                        />
+                    );
+                } ) }
             </div>
         </div>
     );
 }
 
 export default CartPage;
+
